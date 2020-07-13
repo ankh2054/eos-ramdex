764985239d91505ea360b40f2e1e15ed2415b765
Charles Holtzkampf <charles.holtzkampf@gmail.com>
2020-07-13 10:47:31 +0100

    docker file updates

--- files/start.sh
+++ files/start.sh
@@ -58,10 +58,9 @@ echo "creating initdb.sql file"
     SELECT create_hypertable('wax.candles10s', 'timestamp');
 EOF
 
-tail -f logfile | while read LOGLINE
+tail -f /app/logfile | while read LOGLINE
 do   
-    [[ "${LOGLINE}" == *"database system is ready to accept connections"* ]] && echo "DB is ready" &&  \
-    ps -aux | ps axf | grep "tail"  | grep -v grep | awk '{print "kill -9 " $1}' | sh
+    [[ ${LOGLINE} == *"database system is ready to accept connections"* ]] && echo "DB is ready" && pkill -P $$ tail
 done
 
 echo "installing DB: ${DB_DATABASE}" 
