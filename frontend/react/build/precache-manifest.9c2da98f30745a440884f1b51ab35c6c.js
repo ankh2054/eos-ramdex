5e76c6560efe65d87dfb8abce5da34fe9e34203c
Charles Holtzkampf <charles.holtzkampf@gmail.com>
2020-07-30 21:42:12 +0100

    react front change to hooks and materialui theme

---
+++ frontend/react/build/precache-manifest.9c2da98f30745a440884f1b51ab35c6c.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2a8efe0f806992d7bc4153732ebb3c4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c3bab5520cc38420dd34",
+    "url": "/static/css/2.586a6272.chunk.css"
+  },
+  {
+    "revision": "730d89c3084a1bd8d2ca",
+    "url": "/static/css/main.bd202b70.chunk.css"
+  },
+  {
+    "revision": "c3bab5520cc38420dd34",
+    "url": "/static/js/2.7f854d1d.chunk.js"
+  },
+  {
+    "revision": "a192fb9eacc0510352177058117eabea",
+    "url": "/static/js/2.7f854d1d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "730d89c3084a1bd8d2ca",
+    "url": "/static/js/main.f263dc9a.chunk.js"
+  },
+  {
+    "revision": "0200fbc665185d8b0682",
+    "url": "/static/js/runtime-main.a56d8364.js"
+  },
+  {
+    "revision": "01798bc13e33afc36a52f2826638d386",
+    "url": "/static/media/Pe-icon-7-stroke.01798bc1.ttf"
+  },
+  {
+    "revision": "71394c0c7ad6c1e7d5c77e8ac292fba5",
+    "url": "/static/media/Pe-icon-7-stroke.71394c0c.eot"
+  },
+  {
+    "revision": "b38ef310874bdd008ac14ef3db939032",
+    "url": "/static/media/Pe-icon-7-stroke.b38ef310.woff"
+  },
+  {
+    "revision": "c45f7de008ab976a8e817e3c0e5095ca",
+    "url": "/static/media/Pe-icon-7-stroke.c45f7de0.svg"
+  },
+  {
+    "revision": "ef96b0fd01d526cad7e40ddb5dd2a2c1",
+    "url": "/static/media/bg1.ef96b0fd.jpg"
+  },
+  {
+    "revision": "456b47bce336ad14b0da2b679a1ea9e7",
+    "url": "/static/media/bg2.456b47bc.jpg"
+  },
+  {
+    "revision": "bade0f805119c4e72979c6455fec5e1f",
+    "url": "/static/media/bg3.bade0f80.jpg"
+  },
+  {
+    "revision": "ae3c874d0f8ad41aee2a32dae5fab77b",
+    "url": "/static/media/bg4.ae3c874d.jpg"
+  },
+  {
+    "revision": "d67305229d9cbfe677b1498d5941d5e0",
+    "url": "/static/media/bg5.d6730522.jpg"
+  },
+  {
+    "revision": "30c7813285e31015da0183041ff7b6a9",
+    "url": "/static/media/people-1.30c78132.jpg"
+  },
+  {
+    "revision": "42385d5f2cc1c06d2771c2ff6f988837",
+    "url": "/static/media/people-3.42385d5f.jpg"
+  },
+  {
+    "revision": "705ba8f4eed53a18dbbcbed050f6ea2d",
+    "url": "/static/media/sentnl-logo.705ba8f4.svg"
+  }
+]);
